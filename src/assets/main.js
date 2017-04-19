8ec009ccc1695905c5625161e6b0c8a81e334b88
Katie Mathews <hm15@MacBook-Pro-3.local>
2017-04-18 22:50:14 -0500

    challenges complete

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,23 @@
 $(function() {
 
   // your code will go here
+  $.ajax('https://www.codeschool.com/users/ktmathews.json',{
+      dataType: 'jsonp',
+      success: function (response){
+          console.log('inside the success');
+          var completed = response.courses.completed;
+          console.log(completed);
+          for (i = 0; i < completed.length; i++){
+              var course = completed[i];
+              var htmlString = '<div class="course">'+
+                '<h3>' + course.title + '</h3>' +
+                '<img src="' + course.badge + '" />' +
+                '<a href="' + course.url + '" target="_blank" class="btn btn-primary">See Course</a>' +
+                '</div>';
+
+              $('#badges').append(htmlString);
+          }
+      }
+  });
 
 });
